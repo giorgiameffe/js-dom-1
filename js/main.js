9b0ec8ffe7874c297272bffd9987530ab2eaa9af
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-04 17:41:34 +0100

    feat: add button and white-lamp img

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+// inserimento immagine
+
+const pictureElement = document.querySelector('.lamp-picture');
+console.log(pictureElement);
+pictureElement.src = '../img/white_lamp.png';
+
+// inserimento bottone 
+
+const btnElement = document.querySelector('.power-button');
+console.log(btnElement);
